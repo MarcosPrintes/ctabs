2c528999793990c26bfcba09862df104d6055bae
Marcos Printes <marcospprintes.jc@gmail.com>
2025-04-11 00:56:59 -0300

    finish endpoint api/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,35 @@
 import database from "infra/database";
 
+/**
+ * versão do postgres
+ *
+ * conexões máximas
+ *
+ * conexões usadas
+ */
 async function status(request, response) {
-  const res = await database.query("SELECT 1 + 1;");
+  const serverVersion = await database.query("SHOW server_version;");
+  const postgresVersion = serverVersion.rows[0].server_version;
+
+  const maxConnectionsShow = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsShow.rows[0].max_connections);
+
+  const updatedAt = new Date().toISOString();
+
+  const dbName = process.env.POSTGRES_DB;
+
+  const currentConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [dbName],
+  });
 
   response.status(200).send({
-    status: "OKs",
+    status: "database is running",
+    updated_at: updatedAt,
+    database: {
+      postgres_version: postgresVersion,
+      max_connections: maxConnections,
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("Should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody).toEqual({
+    status: "database is running",
+    updated_at: parseUpdatedAt,
+    database: {
+      postgres_version: "16.8",
+      max_connections: 100,
+    },
+  });
 });
