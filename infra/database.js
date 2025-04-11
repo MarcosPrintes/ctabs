df68696e8a1ce1defc3d1a9daa8eeeac6ef6a5b1
Marcos Printes <marcospprintes.jc@gmail.com>
2025-04-11 00:55:51 -0300

    refact

--- infra/database.js
+++ infra/database.js
@@ -3,23 +3,23 @@ import pg from "pg";
 const { Client } = pg;
 
 async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+  await client.connect();
+
   try {
-    const client = new Client({
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-    });
-
-    await client.connect();
-
     const res = await client.query(queryObject);
-
-    await client.end();
     return res;
   } catch (error) {
     console.log("ERROR: DB CONNECTION:", error);
+  } finally {
+    await client.end();
+    console.log("QUERY: FINALLY DISCONNECTED:", queryObject);
   }
 }
 
