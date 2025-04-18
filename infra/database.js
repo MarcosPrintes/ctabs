3a556fa7f61e04552c821440da4ecc4719ec8b3e
Marcos Printes <marcospprintes.jc@gmail.com>
2025-04-18 17:33:40 -0300

    update  to throw error

--- infra/database.js
+++ infra/database.js
@@ -3,19 +3,13 @@ import pg from "pg";
 const { Client } = pg;
 
 async function query(queryObject) {
-  // const client = new Client(
-  //   `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
-  // );
-
-  console.log("======>", process.env.DB_URL);
-  // const client = new Client(process.env.DB_URL);
-
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV !== "development",
   });
 
   try {
@@ -23,10 +17,9 @@ async function query(queryObject) {
     const res = await client.query(queryObject);
     return res;
   } catch (error) {
-    console.log("ERROR: DB CONNECTION:", error);
+    throw error;
   } finally {
     await client.end();
-    console.log("QUERY: FINALLY DISCONNECTED:", queryObject);
   }
 }
 
